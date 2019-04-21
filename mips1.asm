a148ceae052cb4def5d2d9cee5338825a4e68cbd
Najeeb Aqel <najeebmms@hotmail.com>
2019-04-21 23:49:56 +0300

    sorting

--- mips1.asm
+++ mips1.asm
@@ -75,3 +75,55 @@
 	li $v0,2
 	mov.s $f12,$f8
 	syscall
+	
+	#sorting array
+	main:
+    	la  $t0, array      # Copy the base address of your array into $t1
+    	add $t0, $t0, 48    # 4 bytes per int * 10 ints = 40 bytes                              
+	outterLoop:             # Used to determine when we are done iterating over the Array
+    	add $t1, $0, $0     # $t1 holds a flag to determine when the list is sorted
+    	la  $a0, array      # Set $a0 to the base address of the Array
+	innerLoop:                  # The inner loop will iterate over the Array checking if a swap is needed
+    	l.s  $f20, 0($a0)         # sets $t0 to the current element in array
+    	l.s   $f21, 4($a0)         # sets $t1 to the next element in array
+    	#slt  $t5, $t2, $t3       # $t5 = 1 if $t0 < $t1
+    	#beq $t5, $0, continue   # if $t5 = 1, then swap them
+    	c.lt.s $f20,$f21       # if the following value is greater, swap them
+	bc1t continue
+    	add $t1, $0, 1          # if we need to swap, we need to check the list again
+    	s.s  $f20, 4($a0)         # store the greater numbers contents in the higher position in array (swap)
+    	s.s  $f21, 0($a0)         # store the lesser numbers contents in the lower position in array (swap)
+	continue:
+    	addi $a0, $a0, 4            # advance the array to start at the next location from last time
+    	bne  $a0, $t0, innerLoop    # If $a0 != the end of Array, jump back to innerLoop
+    	bne  $t1, $0, outterLoop    # $t1 = 1, another pass is needed, jump back to outterLoop
+    	
+	
+	
+	# print the values of the array 
+	
+	addi $t0,$zero, 0
+	addi $t1,$zero, 1
+	
+	# print new line 
+	li $v0,4
+	la $a0, space   
+	syscall
+	
+	print:
+	bgt $t1, 12, done
+	li $v0,2
+	l.s $f12,array($t0)
+	syscall
+	# print new line 
+	li $v0,4
+	la $a0, space   
+	syscall
+	addi $t0,$t0, 4
+	addi $t1,$t1, 1
+	j print
+	
+	done:
+	
+	
+	
