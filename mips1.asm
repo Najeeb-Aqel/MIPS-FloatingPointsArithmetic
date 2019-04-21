50335f0a91ed5683173920ae1dea0ba3a56ce923
Najeeb Aqel <najeebmms@hotmail.com>
2019-04-21 20:15:17 +0300

    counting positive and negative numbers

--- mips1.asm
+++ mips1.asm
@@ -1,5 +1,5 @@
 .data
-	array:     .space 80
+	array:     .space 100
 	message:   .asciiz "Enter 12 floating point numbers:"
 	zeroFloat: .float 0.0
 	
@@ -19,7 +19,7 @@
 	addi $t0,$zero, 0
 	
 	reading:
-	bgt $t1, 10, exit
+	bgt $t1, 12, exit
 	li $v0, 6
 	syscall
 	swc1 $f0, array($t0) # $t0 = offset
